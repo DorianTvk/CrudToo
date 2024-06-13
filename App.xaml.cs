a208975f03166452f569d2e253740d946ec7535f
Opilane TTHK <opilane@tthk.ee>
2024-06-13 11:56:11 +0300

    commit1

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace CrudToo
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
